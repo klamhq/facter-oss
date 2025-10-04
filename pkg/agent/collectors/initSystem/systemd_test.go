bc9dc9df2b3d37d6d391ec4438ec508e8ff0c3cb
lengletk <kevin.lenglet@gmail.com>
2025-10-04 15:18:16 +0200

    fix: systemd test in ci

--- pkg/agent/collectors/initSystem/systemd_test.go
+++ pkg/agent/collectors/initSystem/systemd_test.go
@@ -1,28 +1,63 @@
 package initSystem
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGatherSystemdInfoFail(t *testing.T) {
+func TestGatherSystemdInfoFailDarwin(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		logger := logrus.New()
+		s, err := GatherSystemdInfo(logger)
+		assert.Error(t, err)
+		assert.Nil(t, s)
+	}
 	logger := logrus.New()
 	s, err := GatherSystemdInfo(logger)
 	assert.Error(t, err)
 	assert.Nil(t, s)
 }
-
-func TestListAllServicesFail(t *testing.T) {
+func TestGatherSystemdInfo(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("no systemd in darwin")
+	}
 	logger := logrus.New()
-	s, err := listAllServices(logger)
-	assert.Error(t, err)
+	s, err := GatherSystemdInfo(logger)
+	assert.NoError(t, err)
 	assert.Nil(t, s)
 }
 
-func TestGetServiceDetailsFail(t *testing.T) {
-	s, err := getServiceDetails("nginx")
-	assert.Error(t, err)
+func TestListAllServicesFail(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		logger := logrus.New()
+		s, err := listAllServices(logger)
+		assert.Error(t, err)
+		assert.Nil(t, s)
+	}
+}
+
+func TestListAllServices(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("no systemd in darwin")
+	}
+	logger := logrus.New()
+	s, err := listAllServices(logger)
+	assert.NoError(t, err)
 	assert.Nil(t, s)
+
+}
+
+func TestGetServiceDetails(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		s, err := getServiceDetails("nginx")
+		assert.Error(t, err)
+		assert.Nil(t, s)
+	} else {
+		s, err := getServiceDetails("nginx")
+		assert.NoError(t, err)
+		assert.Nil(t, s)
+	}
 }
