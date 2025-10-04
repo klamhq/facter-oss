8b61dce8ed72c90960bc12cb092adda1fc8b0755
lengletk <kevin.lenglet@gmail.com>
2025-10-04 20:27:03 +0200

    feat: refacto package_update for use mock

--- pkg/agent/collectors/packages/pac_updates.go
+++ pkg/agent/collectors/packages/pac_updates.go
@@ -2,34 +2,32 @@ package packages
 
 import (
 	"context"
-	"os"
-	"os/exec"
 	"regexp"
 	"strings"
+
+	"github.com/klamhq/facter-oss/pkg/utils"
 )
 
-// GetPacmanUpgradableMap récupère les paquets à mettre à jour (nom -> version dispo)
-func GetPacmanUpgradableMap(ctx context.Context) (map[string]string, error) {
-	cmd := exec.CommandContext(ctx, "pacman", "-Qu")
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "LANG=C")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return nil, err
-	}
+var pacmanRE = regexp.MustCompile(`^(?P<name>[^\s]+)\s+(?P<version>[^\s]+)\s+->\s+(?P<new>[^\s]+)$`)
 
-	// Exemple : iproute2 6.15.0-1 -> 6.16.0-1
-	re := regexp.MustCompile(`^(?P<name>[^\s]+)\s+(?P<version>[^\s]+)\s+->\s+(?P<new>[^\s]+)$`)
+func parsePacmanOutput(out []byte) map[string]string {
 	lines := strings.Split(string(out), "\n")
 	result := make(map[string]string)
-
 	for _, line := range lines {
-		match := re.FindStringSubmatch(line)
+		match := pacmanRE.FindStringSubmatch(line)
 		if match != nil {
 			name := match[1]
 			newVersion := match[3]
 			result[name] = newVersion
 		}
 	}
-	return result, nil
+	return result
+}
+
+func GetPacmanUpgradableMap(ctx context.Context) (map[string]string, error) {
+	out, err := utils.RunCmd(ctx, "pacman", "-Qu")
+	if err != nil {
+		return nil, err
+	}
+	return parsePacmanOutput(out), nil
 }
